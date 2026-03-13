f8c08d0678b7cde177e29e45ebea50b049d461e6
tblanchardviveo <tblanchard@viveo-patrimoine.fr>
2026-03-13 07:10:26 +0100

    Create AvisPage.jsx

---
+++ src/pages/AvisPage.jsx
@@ -0,0 +1,32 @@
+import AvisClient from '../components/AvisClient'
+
+export default function AvisPage() {
+  return (
+    <div style={{
+      minHeight: '70vh',
+      padding: '40px 20px',
+      fontFamily: "'Raleway', sans-serif"
+    }}>
+      <div style={{ textAlign: 'center', marginBottom: 20 }}>
+        <h1 style={{
+          fontSize: 32,
+          fontWeight: 700,
+          color: '#fff',
+          marginBottom: 12
+        }}>
+          {"Avis clients"}
+        </h1>
+        <p style={{
+          fontSize: 15,
+          color: 'rgba(255,255,255,0.5)',
+          maxWidth: 520,
+          margin: '0 auto',
+          lineHeight: 1.7
+        }}>
+          {"D\u00e9couvrez les t\u00e9moignages de nos clients accompagn\u00e9s dans leur projet immobilier."}
+        </p>
+      </div>
+      <AvisClient />
+    </div>
+  )
+}
