53a7368a49b68cb8d860d46019daa32395f6b251
tblanchardviveo <tblanchard@viveo-patrimoine.fr>
2026-03-17 00:38:12 +0100

    feat: App - route /etudes-enfants + footer lien

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ import LoiDenormandie from './pages/fiscal/LoiDenormandie'
 import LoiJeanbrun from './pages/fiscal/LoiJeanbrun'
 import LmnpGere from './pages/fiscal/LmnpGere'
 import Retraite from './pages/Retraite'
+import EtudesEnfants from './pages/EtudesEnfants'
 
 const WP = 'https://viveopromotion-t3jrcqwfw3.live-website.com'
 
@@ -146,7 +147,7 @@ export default function App() {
                         <Route path="/monuments-historiques" element={<MonumentsHistoriques />} />
                         <Route path="/loi-denormandie" element={<LoiDenormandie />} />
                         <Route path="/loi-jeanbrun" element={<LoiJeanbrun />} />
-                      <Route path="/lmnp-gere" element={<LmnpGere />} />               <Route path="/retraite" element={<Retraite />} />
+                      <Route path="/lmnp-gere" element={<LmnpGere />} />               <Route path="/retraite" element={<Retraite />} /> <Route path="/etudes-enfants" element={<EtudesEnfants />} />
         </Routes>
       </main>
       <Footer />
