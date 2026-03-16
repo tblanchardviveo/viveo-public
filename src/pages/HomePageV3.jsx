cc87438c51fb10b855e472f8530b1e7c37e665df
tblanchardviveo <tblanchard@viveo-patrimoine.fr>
2026-03-16 22:23:51 +0100

    feat: HomePageV3 - assemblage 9 sections

---
+++ src/pages/HomePageV3.jsx
@@ -0,0 +1,25 @@
+import HeroV3 from '../components/home-v3/HeroV3'
+import ManifestoV3 from '../components/home-v3/ManifestoV3'
+import ChiffresV3 from '../components/home-v3/ChiffresV3'
+import ProgrammesV3 from '../components/home-v3/ProgrammesV3'
+import DispostifsV3 from '../components/home-v3/DispostifsV3'
+import BanquesV3 from '../components/home-v3/BanquesV3'
+import TemoignagesV3 from '../components/home-v3/TemoignagesV3'
+import ProcessusV3 from '../components/home-v3/ProcessusV3'
+import CtaV3 from '../components/home-v3/CtaV3'
+
+export default function HomePageV3() {
+  return (
+    <>
+      <HeroV3 />
+      <ManifestoV3 />
+      <ChiffresV3 />
+      <ProgrammesV3 />
+      <DispostifsV3 />
+      <BanquesV3 />
+      <TemoignagesV3 />
+      <ProcessusV3 />
+      <CtaV3 />
+    </>
+  )
+}
