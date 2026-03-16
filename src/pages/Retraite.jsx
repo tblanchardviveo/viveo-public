d800eb10041f63860cacb10c7f0cb4327f58f5ca
tblanchardviveo <tblanchard@viveo-patrimoine.fr>
2026-03-16 23:50:27 +0100

    feat: Retraite page - assemblage composants

---
+++ src/pages/Retraite.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from 'react'
+import HeroRetraite from '../components/retraite/HeroRetraite'
+import VeriteRetraite from '../components/retraite/VeriteRetraite'
+import ProfilsRetraite from '../components/retraite/ProfilsRetraite'
+import SimulateurRetraite from '../components/retraite/SimulateurRetraite'
+import SolutionViveo from '../components/retraite/SolutionViveo'
+import CtaRetraite from '../components/retraite/CtaRetraite'
+
+export default function Retraite() {
+  useEffect(() => {
+    window.scrollTo(0, 0)
+    document.title = 'Pr\u00e9parer ma retraite | VIVEO Patrimoine'
+  }, [])
+
+  return (
+    <div style={{ background: 'var(--navy-deep)' }}>
+      <HeroRetraite />
+      <VeriteRetraite />
+      <ProfilsRetraite />
+      <SimulateurRetraite />
+      <SolutionViveo />
+      <CtaRetraite />
+    </div>
+  )
+}
