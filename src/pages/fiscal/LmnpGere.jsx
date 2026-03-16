5d4c7d9f3413a599a51278e0ae354f7607e010ed
tblanchardviveo <tblanchard@viveo-patrimoine.fr>
2026-03-16 22:04:52 +0100

    Create LmnpGere.jsx

---
+++ src/pages/fiscal/LmnpGere.jsx
@@ -0,0 +1,17 @@
+import HeroLmnpGere from '../../components/lmnp-gere/HeroLmnpGere'
+import ArgumentsChocs from '../../components/lmnp-gere/ArgumentsChocs'
+import DemographieStats from '../../components/lmnp-gere/DemographieStats'
+import TypesResidences from '../../components/lmnp-gere/TypesResidences'
+import SimulateurLmnp from '../../components/lmnp-gere/SimulateurLmnp'
+
+export default function LmnpGere() {
+  return (
+    <>
+      <HeroLmnpGere />
+      <ArgumentsChocs />
+      <DemographieStats />
+      <TypesResidences />
+      <SimulateurLmnp />
+    </>
+  )
+}
