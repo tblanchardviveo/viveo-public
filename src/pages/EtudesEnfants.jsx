4e615d3d022cc27cd7bdcb1dbd2f9df43271ffc5
tblanchardviveo <tblanchard@viveo-patrimoine.fr>
2026-03-17 00:30:59 +0100

    feat: EtudesEnfants - page orchestrateur

---
+++ src/pages/EtudesEnfants.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from 'react'
+import HeroEtudes from '../components/etudes/HeroEtudes'
+import CoutEtudes from '../components/etudes/CoutEtudes'
+import ProfilsEtudes from '../components/etudes/ProfilsEtudes'
+import SimulateurEtudes from '../components/etudes/SimulateurEtudes'
+import SolutionEtudes from '../components/etudes/SolutionEtudes'
+import CtaEtudes from '../components/etudes/CtaEtudes'
+
+export default function EtudesEnfants() {
+  useEffect(() => {
+    window.scrollTo(0, 0)
+    document.title = 'Financer les \u00e9tudes de vos enfants | VIVEO Patrimoine'
+  }, [])
+
+  return (
+    <div style={{ background: 'var(--navy-deep)' }}>
+      <HeroEtudes />
+      <CoutEtudes />
+      <ProfilsEtudes />
+      <SimulateurEtudes />
+      <SolutionEtudes />
+      <CtaEtudes />
+    </div>
+  )
+}
