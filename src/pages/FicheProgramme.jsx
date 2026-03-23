e252c6513050f13bc915166eb4334af05e13e550
tblanchardviveo <tblanchard@viveo-patrimoine.fr>
2026-03-23 18:37:32 +0100

    fix: mise à jour clé Google Maps API (VIVEO Analytics)

--- src/pages/FicheProgramme.jsx
+++ src/pages/FicheProgramme.jsx
@@ -132,7 +132,7 @@ export default function FicheProgramme() {
             </div>)}
           {(p.latitude && p.longitude) && (
             <div style={{ marginTop: 40 }}><Eyebrow>LOCALISATION</Eyebrow>
-              <iframe title="carte" src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyAF-pzcNWC0rRmKVRHrGtRlKT1ro2gXlHw&q=${p.latitude},${p.longitude}&zoom=15&maptype=roadmap`} style={{ width: '100%', height: 300, border: '1.5px solid #E5E3E0', borderRadius: 12 }} />
+              <iframe title="carte" src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyAVk5IkxXQYCzTKfRUkBbTIVLw_zQVCZSc&q=${p.latitude},${p.longitude}&zoom=15&maptype=roadmap`} style={{ width: '100%', height: 300, border: '1.5px solid #E5E3E0', borderRadius: 12 }} />
             </div>)}
 
                     {p.ville && (
